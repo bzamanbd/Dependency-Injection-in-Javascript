8716d4bf5b04752705a93d5718937a66ba0f73d6
bzamanbd <riponzamanbd@gmail.com>
2024-06-14 12:02:05 +0600

    Done

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
 class Database{ 
+    get(){ 
+        return 'getting data from database'
+    }
+
+    store(data){ 
+        return `storing ${JSON.stringify(data)} to database`
+    }
 
 }
 
 
 class User{ 
-    
+    constructor(database){ 
+        this.db = database
+    }
+
+    find(){ 
+        return this.db.get()
+    }
+
+    create(data){ 
+        return this.db.store(data)
+    }
 }
+
+
+const d = new Database()
+const u = new User(d) 
+
+console.log(u.create({"city":"dhaka"}));
+console.log(u.find());
